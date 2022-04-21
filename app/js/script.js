6647e4b54ea0eb9e712e18e8894d0b6e9e299802
Dee Desai <devarshidesai2019@gmail.com>
2022-04-21 23:52:11 +0100

    Added mobile menu and animation to the burger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,22 @@
 const btnhamburger = document.querySelector('#btnhamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElms = document.querySelectorAll('.has-fade');
 
 btnhamburger.addEventListener('click', function() {
     console.log('click hamburger');
 
     if(header.classList.contains('open')) {
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElms.forEach(function(element){
+          element.classList.remove('fade-in');
+          element.classList.add('fade-out');
+        });
     } else {
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElms.forEach(function(element){
+          element.classList.add('fade-in');
+          element.classList.remove('fade-out');
+        });
     }
 });
