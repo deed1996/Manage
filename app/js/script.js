6da75b6ed2dc4eb91526bd202375701b9235bcaa
Dee Desai <devarshidesai2019@gmail.com>
2022-04-25 00:39:42 +0100

    finished the main section and fixed styling issues

--- app/js/script.js
+++ app/js/script.js
@@ -3,20 +3,20 @@ const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElms = document.querySelectorAll('.has-fade');
 
-btnhamburger.addEventListener('click', function() {
-    console.log('click hamburger');
+btnhamburger.addEventListener('click', function () {
+  console.log('click hamburger');
 
-    if(header.classList.contains('open')) {
-        header.classList.remove('open');
-        fadeElms.forEach(function(element){
-          element.classList.remove('fade-in');
-          element.classList.add('fade-out');
-        });
-    } else {
-        header.classList.add('open');
-        fadeElms.forEach(function(element){
-          element.classList.add('fade-in');
-          element.classList.remove('fade-out');
-        });
-    }
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+    fadeElms.forEach(function (element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+  } else {
+    header.classList.add('open');
+    fadeElms.forEach(function (element) {
+      element.classList.add('fade-in');
+      element.classList.remove('fade-out');
+    });
+  }
 });
