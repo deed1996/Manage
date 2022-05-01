c1e2f00aeb80cb3b61fde840728d0d02fa81a474
Dee Desai <devarshidesai2019@gmail.com>
2022-05-01 01:01:15 +0100

    added slider to the testimonials section

--- app/js/script.js
+++ app/js/script.js
@@ -20,3 +20,31 @@ btnhamburger.addEventListener('click', function () {
     });
   }
 });
+
+
+const slider= tns({container:".testimonials_slider",
+  loop:!0,
+  items:1,
+  gutter:20,
+  slideBy:"page",
+  nav:true,
+  navPosition: "bottom",
+  autoplay:!0,
+  speed:600,
+  autoplayButtonOutput:!1,
+  mouseDrag:!0,
+  controlsContainer: "#customize-controls",
+  edgePadding: 0,
+  prevButton:false,
+  nextButton: false,
+  controls: false,
+  responsive:{
+    640:{items:1},
+    768:{
+      items:2.5,
+      nav: false,
+      speed:800,
+      autoplayHoverPause: true
+    }
+  }
+  });
